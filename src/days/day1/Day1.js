5a6d0801838f2058ce47a33a25228500e7031dd1
jihun.kang <kjhss2@nate.com>
2021-09-23 17:46:31 +0900

    * day1 commit

---
+++ src/days/day1/Day1.js
@@ -0,0 +1,47 @@
+import './day1.css';
+
+export default function Day1() {
+  return (
+    <div className="contaniner">
+
+      {/* Step1 : Button Style */}
+      <div className="box">
+        <div className="box_button">
+          Button1
+        </div>
+        <div className="box_button">
+          Button2
+        </div>
+        <div className="box_button">
+          Button3
+        </div>
+      </div>
+
+      {/* Step2 : Input Style */}
+      <div className="box">
+        <div className="step2">
+          ID : <input />
+          PW : <input />
+        </div>
+      </div>
+
+      {/* Step3 : Input Radio Style */}
+      <div className="box">
+        <div className="step3">
+          On <input />
+          Off <input />
+        </div>
+      </div>
+
+      {/* Step4 : Input checkbox Style */}
+      <div className="box">
+        <div className="step4">
+          one <input />
+          two <input />
+          three <input />
+        </div>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/days/day1/Day1.md
@@ -0,0 +1,27 @@
+# CSS Mission
+
+You must modify the day1.css file to satisfy the following conditions.
+
+Note The image file is day1.png
+
+## Mission 1 : Button Style Modify(Keyword : padding, margin, flexbox, border-radius)
+
+Refer to the day1.png image file and write it.
+
+## Mission 2 : You Make id/password input Style(Keyword : input)
+
+Refer to the day1.png image file and write it.
+
+## Mission 3 : You Make radio Style(Keyword : input type radio)
+
+Refer to the day1.png image file and write it.
+
+## Mission 4 : You Make Checkbox Style(Keyword : input type checkbox)
+
+Refer to the day1.png image file and write it.
+
+## Mission 5 : Use Response CSS(Keyword : @media query)
+
+  1) browser width 320px ~ 767px : background color is green
+  2) browser width 768px ~ 1199px : background color is yellow
+  3) browser width 1200px ~ : background color is gray
