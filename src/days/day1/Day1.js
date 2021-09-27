5e9f0bd614a3be2b30b865c7a35ee73c3fc8e990
DESKTOP-92VNL0O\ITGROUND-02 <bettynigat@gmail.com>
2021-09-27 14:39:51 +0900

    second commit

--- src/days/day1/Day1.js
+++ src/days/day1/Day1.js
@@ -7,39 +7,41 @@ export default function Day1() {
 
       {/* Step1 : Button Style */}
       <div className="box">
-        <div className="box_button">
-          Button1
-        </div>
-        <div className="box_button">
-          Button2
-        </div>
-        <div className="box_button">
-          Button3
+        <div className="button">
+          <div className="box_button">
+            Button1
+          </div>
+          <div className="box_button">
+            Button2
+          </div>
+          <div className="box_button">
+            Button3
+          </div>
         </div>
       </div>
 
       {/* Step2 : Input Style */}
       <div className="box">
         <div className="step2">
-          ID : <input />
-          PW : <input />
+          ID : <input type="text"/>
+          PW : <input type="password"/>
         </div>
       </div>
 
       {/* Step3 : Input Radio Style */}
       <div className="box">
         <div className="step3">
-          On <input />
-          Off <input />
+          On <input type="radio" name="step3" value="on" />
+          Off <input type="radio" name="step3" value="off"/>
         </div>
       </div>
 
       {/* Step4 : Input checkbox Style */}
       <div className="box">
         <div className="step4">
-          one <input />
-          two <input />
-          three <input />
+          one <input type="checkbox" name="step4" value="one"/>
+          two <input type="checkbox" name="step4" value="one"/>
+          three <input type="checkbox" name="step4" value="one"/>
         </div>
       </div>
 
