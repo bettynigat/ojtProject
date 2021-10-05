560c4afe9888a76a2532441f402dad9fd7da94f9
DESKTOP-92VNL0O\ITGROUND-02 <bettynigat@gmail.com>
2021-10-05 17:07:12 +0900

    Day6: ShowModal completed

--- src/days/day5/componets/news/News.js
+++ src/days/day5/componets/news/News.js
@@ -11,36 +11,53 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
+function NewsHeader(props) {
 
-    
-class NewsHeader extends React.Component {
+  const [show, setShow] = useState(false);
 
-  state = {
-    show: false
+  const showModal = () => {
+    setShow(true)
   };
 
-  showModal = e => {
-    this.setState({
-      show: true
-    });
+  const closeModal = () => {
+    setShow(false)
   };
 
-  handleAfterOpen= ()=>{
+  const [titleInput, setTitleInput] = useState('');
+  const [articleInput, setArticleInput] = useState('');
 
+  const handleTitleChange = (e) => {
+    setTitleInput(e.currentTarget.value)
   }
 
-  render(){
+  const handleArticleChange = (e) => {
+    setArticleInput(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTask(titleInput, articleInput);
+    setTitleInput("");
+    setArticleInput("");
+  }
+
   return (
     <div className="add-news">
-      <button onClick={e => {this.showModal();}}>Add News</button>
-      <Modal isOpen={this.state.show} onAfterOpen={this.handleAfterOpen} className="Modal">
+      <button onClick={() => { showModal() }}>Add News</button>
+      <Modal isOpen={show} className="Modal">
         <h1>Add News</h1>
-        Title: <input type="text" />
-        Article: <input type="text"/>
+        <div className="modal-inputs">
+          Title: <input type="text" value={titleInput} onChange={handleTitleChange} placeholder="Add an title..." />
+          Article: <input type="text" value={articleInput} onChange={handleArticleChange} placeholder="Add an article..." />
+        </div>
+        <div className="close">
+          <button onClick={() => { closeModal() }}>Close</button>
+          <button onClick={handleSubmit}>Save Changes</button>
+        </div>
+
       </Modal>
     </div>
   )
-  }
 }
 
 function NewsItem(props) {
@@ -87,11 +104,17 @@ function News() {
   ];
   const [listArrays, setToDoList] = useState(data);
 
+  const addTask = (titleInput, articleInput) => {
+    let copy = [...listArrays];
+    copy = [...copy, { id: listArrays.length + 1, article: articleInput, title: titleInput }];
+    setToDoList(copy);
+  }
+
   return (
     <div className="containers">
       <div className="main-pages">
         <div className="news-header">
-          <NewsHeader />
+          <NewsHeader addTask={addTask} />
         </div>
         <div className="display-lists">
           <DisplayNews listArrays={listArrays} />
